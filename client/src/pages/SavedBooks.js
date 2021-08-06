93d8240181bb62f755928838666edcb88c0bfb31
Kevin Henley <75505087+KevinHenleyCode@users.noreply.github.com>
2021-08-06 11:14:35 -0500

    coppied files from old repo to this one

---
+++ client/src/pages/SavedBooks.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
+import Auth from '../utils/auth';
+import { removeBookId } from '../utils/localStorage';
+import { useQuery, useMutation } from '@apollo/client'
+import { GET_ME } from '../utils/queries'
+import { REMOVE_BOOK } from '../utils/mutations'
+
+
+
+const SavedBooks = () => {
+  
+  const { loading, data } = useQuery(GET_ME)
+  const [removeBook] = useMutation(REMOVE_BOOK)
+  const userData = data?.me || {}
+
+  if (!userData.username){
+    return ( <h4>Hold up! Login if you want to view what's on this page.</h4> )
+  }
+
+
+  // create function that accepts the book's mongo _id value as param and deletes the book from the database
+  const handleDeleteBook = async (bookId) => {
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+    if (!token) {
+      return false;
+    }
+
+    try {
+      
+      await removeBook({ variables: { bookId },
+        
+        update: cache => {
+          const data = cache.readQuery({ query: GET_ME })
+          const userData = data.me
+          const savedCache = userData.savedBooks
+          const updatedCache = savedCache.filter((book) => book.bookId !== bookId)
+          userData.savedBooks = updatedCache
+          cache.writeQuery({ query: GET_ME, data: { data: { ...userData.savedBooks } } })
+        }
+      })
+
+      // upon success, remove book's id from localStorage
+      removeBookId(bookId);
+    
+    } catch (err) {
+      
+      console.error(err);
+    }
+  }
+
+  // if data isn't here yet, say so
+  if (loading) {
+    return <h2>PAGE IS LOADING...</h2>;
+  }
+
+  return (
+    <>
+      <Jumbotron fluid className='text-light bg-dark'>
+        <Container>
+          <h1>Viewing saved books!</h1>
+        </Container>
+      </Jumbotron>
+      <Container>
+        <h2>
+          {userData.savedBooks.length
+            ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
+            : 'You have no saved books!'}
+        </h2>
+        <CardColumns>
+          {userData.savedBooks.map((book) => {
+            return (
+              <Card key={book.bookId} border='dark'>
+                {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
+                <Card.Body>
+                  <Card.Title>{book.title}</Card.Title>
+                  <p className='small'>Authors: {book.authors}</p>
+                  <Card.Text>{book.description}</Card.Text>
+                  <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
+                    Delete this Book!
+                  </Button>
+                </Card.Body>
+              </Card>
+            );
+          })}
+        </CardColumns>
+      </Container>
+    </>
+  );
+};
+
+export default SavedBooks;
---
+++ client/src/utils/mutations.js
@@ -0,0 +1,66 @@
+import { gql } from '@apollo/client'
+
+
+
+export const LOGIN = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password){
+           token
+           user {
+               _id
+               username
+           }
+        }
+    }
+`
+
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`
+
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($book: SaveBookIn!) {
+        saveBook(book: $book) {
+            username
+            email
+            bookCount
+            savedBooks {
+                authors
+                description
+                title
+                bookId
+                image
+                link
+            }
+        }
+    }
+`
+
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: String!) {
+        removeBook(bookId: $bookId) {
+            username
+            email
+            bookCount
+            savedBooks {
+                authors
+                description
+                title
+                bookId
+                image
+                link
+            }
+        }
+    }
+`
